9bd8520270bbfae2642137ba0fc68021e38365ac
JaredMejia <jared.mejia06@gmail.com>
2021-06-21 17:22:59 -0700

    Fix conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,6 +36,11 @@ extensions = ["recommonmark", "sphinx.ext.autodoc", "sphinx.ext.viewcode"]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
+
+source_suffix = ".rst"
+
+root_doc = "index"
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
