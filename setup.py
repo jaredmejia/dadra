b6dbebbcec8b7de997f9d803135caae55aff8c6f
JaredMejia <jared.mejia06@gmail.com>
2021-06-24 23:46:22 -0700

    Version 0.0.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name="dadra",
     packages=setuptools.find_packages(),
     license="MIT",
-    version="0.0.3",
+    version="0.0.4",
     description="Library for Data-Driven Reachability Analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
