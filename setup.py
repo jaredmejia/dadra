d00b0ccb892c6cfd16e1391df9857885b988ff54
JaredMejia <jared.mejia06@gmail.com>
2021-06-24 22:08:18 -0700

    Added setup.cfg

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from setuptools import find_packages, setup
+import setuptools
 
 setup(
     name="dadra",
-    packages=find_packages(include=["dadra"]),
-    version="0.0.1",
+    packages=setuptools.find_packages(),
+    license="MIT",
+    version="0.0.2",
     description="Library for Data-Driven Reachability Analysis",
     author="Jared Mejia",
     author_email="jaredmejia@berkeley.edu",
     url="https://github.com/JaredMejia/dadra",
-    license="MIT",
     install_requires=["cvxpy", "matplotlib", "numpy", "scipy"],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ),  
+    ),
 )
